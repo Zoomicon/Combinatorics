22267359a6264b402dd25ce557f71c19e915e7c8
birbilis <birbilis@localhost>
2014-09-13 16:44:10 +0300

    - First version, uploaded PCL and WP8 library using code from http://www.codeproject.com/Articles/26050/Permutations-Combinations-and-Variations-using-C-G and additions done for Anagram Windows Phone app (http://www.windowsphone.com/en-us/store/app/anagram/69d9ac02-07fe-48e0-8086-cd9ee1884dd4)

---
+++ Common/CombinatoricsOp.cs
@@ -0,0 +1,46 @@
+﻿//Version: 20111119
+//Author: George Birbilis (birbilis@kagi.com)
+
+using System;
+using System.Collections.Generic;
+
+namespace Facet.Combinatorics
+{
+
+  public static class CombinatoricsOp {
+
+    public enum CombinatoricsOpType
+    {
+      Permutations = 0,
+      PermutationsWithRepetition = 1,
+      //---
+      Combinations = 2,
+      CombinationsWithRepetition = 3,
+      //---
+      Variations = 4,
+      VariationsWithRepetition = 5
+    }
+   
+   public static IMetaCollection<string> GetCombinatoricsOp(IList<string> values, CombinatoricsOpType opType, int take){
+     switch (opType)
+     {
+       case CombinatoricsOpType.Permutations:
+         return new Permutations<string>(values);
+       case CombinatoricsOpType.PermutationsWithRepetition:
+         return new Permutations<string>(values,GenerateOption.WithRepetition);
+       case CombinatoricsOpType.Combinations:
+         return new Combinations<string>(values, take);
+       case CombinatoricsOpType.CombinationsWithRepetition:
+         return new Combinations<string>(values, take, GenerateOption.WithRepetition);
+       case CombinatoricsOpType.Variations:
+         return new Variations<string>(values, take);
+       case CombinatoricsOpType.VariationsWithRepetition:
+         return new Variations<string>(values, take, GenerateOption.WithRepetition);
+       default:
+         throw new NotImplementedException();
+     }
+   }
+
+  }
+  
+}
